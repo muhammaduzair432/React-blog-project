c719ae10687135825894ff461494db9a0692aca6
muhammad uzair <abuzada735@gmail.com>
2025-10-21 19:51:55 +0500

    done

--- blog website/src/App.jsx
+++ blog website/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
       <div className='w-full block'>
         <Header />
         <main>
-        TODO:  <Outlet />
+        {/* TODO:  <Outlet /> */}
         </main>
         <Footer />
       </div>
